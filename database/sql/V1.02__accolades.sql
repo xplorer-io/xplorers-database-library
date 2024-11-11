9b21e698759077e4865e33fb14304fa3a4d5d95a
Aruna Acharya <87629377+ArunaAcharya@users.noreply.github.com>
2024-11-11 19:03:43 +1100

    typo fix

--- database/sql/V1.02__accolades.sql
+++ database/sql/V1.02__accolades.sql
@@ -5,8 +5,9 @@ CREATE TABLE Accolades (
     fullName VARCHAR(255),
     message TEXT, 
     anonymus BOOLEAN,
-    createdDate TIMESTAMP
-);
+    createdDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+
 CREATE INDEX idx_fullName ON Accolades (fullName);
 CREATE INDEX idx_anonymus ON Accolades (anonymus);
 CREATE INDEX idx_createdDate ON Accolades (createdDate);
